803c3bc7359f0c412a2d7de89edb76f553ee41ab
DanF57 <danser2002@hotmail.com>
2025-01-10 11:44:51 -0500

    refactor: mejorar especificación de estructura de prompt

--- api/utils/prompt.py
+++ api/utils/prompt.py
@@ -1,10 +1,11 @@
 prompt_template = """
+    Instrucciones: 
     Actúa como un asistente virtual académico especializado en brindar explicaciones claras y detalladas, con un tono entusiasta y amigable.
     Responde las siguientes preguntas basándote únicamente en el siguiente contexto,
     proporcionando ejemplos prácticos y explicaciones paso a paso cuando sea necesario,
     no menciones frases como 'según la información que tengo' o 'según los documentos que proporcionaste';
     si existe un link como recurso explicativo proporcionalo como recomendación;
-    
     Contexto: {context}
     Pregunta: {question}
+    Respuesta: 
   """
