1f10fe71d930ee4f59ebf4e480d24b3f7d7ff2bc
DanF57 <danser2002@hotmail.com>
2025-01-20 20:42:02 -0500

    feature: mejorar prompt para que no salude repetitivamente

--- api/index.py
+++ api/index.py
@@ -10,6 +10,7 @@ from .utils.prompt import prompt_template
 from pydantic import BaseModel
 from fastapi import FastAPI, Query
 from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 #LangChain
 from langchain_google_genai import ChatGoogleGenerativeAI
@@ -19,7 +20,7 @@ from langchain_pinecone import PineconeVectorStore
 from pinecone import Pinecone
 
 # Cargar variables de entorno
-load_dotenv(".env.local") 
+load_dotenv(".env") 
 PINECONE_API_KEY = os.environ.get("PINECONE_API_KEY")
 GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
 
@@ -31,6 +32,20 @@ app = FastAPI(
     description="API para chatbot con RAG usando Gemini y Pinecone",
     version="1.0.0"
 )
+
+# CORS
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)   
 
 # Modelo de Embeddings
 embeddings = GoogleGenerativeAIEmbeddings(model="models/text-embedding-004")
@@ -44,18 +59,17 @@ vector_store = PineconeVectorStore(index=index, embedding=embeddings, namespace=
 
 # Configurar el modelo de Gemini usando Langchain
 llm = ChatGoogleGenerativeAI(
-    model="gemini-1.5-flash",
+    model="gemini-1.5-flash", #Modelo especializado en resúmenes
     google_api_key=GOOGLE_API_KEY,
     streaming=True,
-    temperature=0.3,
+    temperature=0.4,
 )
 
 
 def stream_rag_response(messages: List[dict]):
     """Devuelve respuestas generadas con RAG como un stream compatible con Vercel AI SDK."""
-    question = messages[0]['content']
-    #print("lista:", messages)
-    #print("question: ", question)
+    #Extraer última pregunta del usuario
+    question = messages[-1]['content']
     
     # Configuración del recuperador
     retriever = vector_store.as_retriever(
@@ -69,8 +83,8 @@ def stream_rag_response(messages: List[dict]):
         f"Página {doc.metadata.get('page', 'desconocida')}: {doc.page_content}"
         for doc in docs
     ]
+    #Formatear como una sola cadena de texto
     context = "\n".join(context_items)
-    print(context)
 
     # Construir el mensaje con contexto y pregunta
     input_data = {"context": context, "question": question}
@@ -81,9 +95,11 @@ def stream_rag_response(messages: List[dict]):
 
     # Iterar sobre los fragmentos generados y transmitirlos en formato Vercel AI SDK
     for chunk in result_stream:
-        yield "0:{text}\n".format(text=json.dumps(chunk.content))
+        # Cada chunk será un JSON válido en el formato esperado
+        yield '0:{text}\n'.format(text=json.dumps(chunk.content))
 
-    yield 'e:{{"finishReason":"stop","usage":{{"promptTokens":0,"completionTokens":0}},"isContinued":false}}\n'
+    # Mensaje de cierre del stream
+    yield '2:[{"finishReason":"stop","usage":{"promptTokens":0,"completionTokens":0},"isContinued":false}]\n'
 
 
 #API
