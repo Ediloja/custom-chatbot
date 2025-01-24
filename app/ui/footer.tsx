09c4361d9e8c0365306fa4e512e2aff53152bad8
rjmasache <royerjmasache@gmail.com>
2025-01-24 16:59:33 -0500

    ops: Modificación de mensajes de UI para deployment

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <p className="mt-4 text-center text-xs text-gray-600 md:text-sm">
-            UTPL Assistant puede cometer errores. Utilízalo con precaución.
+            ShopiaUTPL puede cometer errores. Utilízalo con precaución.
         </p>
     );
 }
